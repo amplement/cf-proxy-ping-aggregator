1afd2a0a631b8eb79deceaae0be98e46a19a12f0
cédric <cedbale@gmail.com>
2025-10-28 16:12:41 +0100

    feat: move uninstall

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo systemctl stop cf_proxy_ping_aggregator.service
+sudo systemctl disable cf_proxy_ping_aggregator.service
+sudo rm /usr/lib/systemd/system/cf_proxy_ping_aggregator.service
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
